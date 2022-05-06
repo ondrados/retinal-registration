5b28d4c7ca8368591ab4aa3c0543586522849d0b
ondrados <ondrejdoskocil@gmail.com>
2022-05-06 14:46:36 +0200

    Added ability to use pretrained model

--- train.py
+++ train.py
@@ -41,18 +41,31 @@ if __name__ == "__main__":
 
     test_loader = DataLoader(test_dataset, batch_size=1, num_workers=0, shuffle=False)
 
+    pretrained = True
     model = siamese_resnet18(1, 7)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Running on {device}")
     model.to(device=device)
+
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    training_loss = []
-    validation_loss = []
+    if pretrained:
+        checkpoint_path = "07_resnet18_MSE_corr/checkpoint-12.pt"
+        checkpoint = torch.load(os.path.join("/content/drive/MyDrive/outputs", checkpoint_path), map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch_check = checkpoint['epoch']
+        training_loss_check = checkpoint['training_loss']
+        validation_loss_check = checkpoint['validation_loss']
 
-    for epoch in range(0, 10):
+    print(f"Running on {device}")
+
+    training_loss = training_loss_check if pretrained else []
+    validation_loss = validation_loss_check if pretrained else []
+    start = epoch_check + 1 if pretrained else 0
+    end = epoch_check + 25 if pretrained else 25
+
+    for epoch in range(start, end):
         model.train()
         running_loss = 0.0
         with tqdm(train_loader, unit="batch") as progress:
@@ -84,7 +97,13 @@ if __name__ == "__main__":
 
         validation_loss.append(val_running_loss / len(test_loader))
 
-        torch.save(model.state_dict(), os.path.join(output_path, f"model-{epoch}.pt"))
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'training_loss': training_loss,
+            'validation_loss': validation_loss
+        }, os.path.join(output_path, f"checkpoint-{epoch}.pt"))
 
         plt.figure()
         plt.plot(training_loss, label="training loss")
