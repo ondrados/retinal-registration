0bb97f9ec51fc3950c4db517751314b5066cc7bc
ondrados <ondrejdoskocil@gmail.com>
2023-05-12 08:00:41 +0200

    Added matching_type switch

--- models.py
+++ models.py
@@ -104,10 +104,10 @@ class FeatureRegression(nn.Module):
 
 class SiameseResNet(nn.Module):
 
-    def __init__(self, in_channels, n_classes, *args, **kwargs):
+    def __init__(self, in_channels, n_classes, matching_type, *args, **kwargs):
         super().__init__()
         self.encoder = ResNetEncoder(in_channels, *args, **kwargs)
-        self.correlation = FeatureCorrelation()
+        self.correlation = FeatureCorrelation(matching_type=matching_type)
         self.regression = FeatureRegression(output_dim=n_classes)
 
     def forward(self, input1, input2):
@@ -124,35 +124,35 @@ class SiameseResNet(nn.Module):
 blocks_sizes = [64, 128, 256, 512]
 
 
-def siamese_resnet18(in_channels, n_classes):
+def siamese_resnet18(in_channels, n_classes, matching_type):
     return SiameseResNet(
-        in_channels, n_classes, block=ResNetBasicBlock, deepths=[2, 2, 2, 2], blocks_sizes=blocks_sizes)
+        in_channels, n_classes, matching_type, block=ResNetBasicBlock, deepths=[2, 2, 2, 2], blocks_sizes=blocks_sizes)
 
 
-def siamese_resnet34(in_channels, n_classes):
+def siamese_resnet34(in_channels, n_classes, matching_type):
     return SiameseResNet(
-        in_channels, n_classes, block=ResNetBasicBlock, deepths=[3, 4, 6, 3], blocks_sizes=blocks_sizes)
+        in_channels, n_classes, matching_type, block=ResNetBasicBlock, deepths=[3, 4, 6, 3], blocks_sizes=blocks_sizes)
 
 
-def siamese_resnet50(in_channels, n_classes):
+def siamese_resnet50(in_channels, n_classes, matching_type):
     return SiameseResNet(
-        in_channels, n_classes, block=ResNetBottleNeckBlock, deepths=[3, 4, 6, 3], blocks_sizes=blocks_sizes
+        in_channels, n_classes, matching_type, block=ResNetBottleNeckBlock, deepths=[3, 4, 6, 3], blocks_sizes=blocks_sizes
     )
 
 
-def siamese_resnet101(in_channels, n_classes):
+def siamese_resnet101(in_channels, n_classes, matching_type):
     return SiameseResNet(
-        in_channels, n_classes, block=ResNetBottleNeckBlock, deepths=[3, 4, 23, 3], blocks_sizes=blocks_sizes
+        in_channels, n_classes, matching_type, block=ResNetBottleNeckBlock, deepths=[3, 4, 23, 3], blocks_sizes=blocks_sizes
     )
 
 
-def siamese_resnet152(in_channels, n_classes):
+def siamese_resnet152(in_channels, n_classes, matching_type):
     return SiameseResNet(
-        in_channels, n_classes, block=ResNetBottleNeckBlock, deepths=[3, 8, 36, 3], blocks_sizes=blocks_sizes)
+        in_channels, n_classes, matching_type, block=ResNetBottleNeckBlock, deepths=[3, 8, 36, 3], blocks_sizes=blocks_sizes)
 
 
 if __name__ == '__main__':
     from torchsummary import summary
 
-    model = siamese_resnet18(1, 7)
+    model = siamese_resnet18(1, 7, "correlation")
     summary(model.cpu(), [(1, 500, 750), (1, 500, 750)])
